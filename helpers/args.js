c0bf38a4701d97ae8e797cd23a69103b7770cd86
Aleksandr <generalovshura9@gmail.com>
2025-09-13 19:49:22 +0300

    created service

--- helpers/args.js
+++ helpers/args.js
@@ -1,9 +1,10 @@
-const getArgs = (args) => {
-    const res = {}
-    const [executer, file, ...rest ] = args;
+export const getArgs = (args) => {
+    const res = {};
+    const [executor, file, ...rest] = args;
+
     rest.forEach((value, index, array) => {
         if (value.charAt(0) == '-') {
-            if(index === array.length -1) {
+            if (index === array.length - 1) {
                 res[value.substring(1)] = true;
             } else if (array[index + 1].charAt(0) != '-') {
                 res[value.substring(1)] = array[index + 1];
@@ -12,7 +13,6 @@ const getArgs = (args) => {
             }
         }
     });
+
     return res;
 };
-
-export { getArgs }
--- weather.js
+++ weather.js
@@ -1,10 +1,23 @@
 #!/usr/bin/env node
 import { getArgs } from "./helpers/args.js";
+import { printHelp } from "./services/log.service.js";
 
 const initCLI = () => {
     const args = getArgs(process.argv);
-    console.log('Parsed arguments:', args);
-    console.log('Raw process.argv:', process.argv);
+    if(args.h) {
+        // Вывод help
+        printHelp()
+    };
+
+    if(args.s) {
+        // Сохранить город
+    };
+
+    if(args.t) {
+        // Сохранить токен
+    };
+
+    // Вывести погоду
 }
 
 initCLI();
